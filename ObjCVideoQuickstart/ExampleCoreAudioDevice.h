8567849b4c7169befe5de1ebaea21d2cc5943954
Chris Eagleston <ceagleston@twilio.com>
2018-01-23 09:33:18 -0800

    Add ExampleCoreAudioDevice skeleton.

---
+++ ObjCVideoQuickstart/ExampleCoreAudioDevice.h
@@ -0,0 +1,12 @@
+//
+//  ExampleCoreAudioDevice.h
+//  ObjCVideoQuickstart
+//
+//  Copyright © 2018 Twilio, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ExampleCoreAudioDevice : NSObject
+
+@end
---
+++ ObjCVideoQuickstart/ExampleCoreAudioDevice.m
@@ -0,0 +1,12 @@
+//
+//  ExampleCoreAudioDevice.m
+//  ObjCVideoQuickstart
+//
+//  Copyright © 2018 Twilio, Inc. All rights reserved.
+//
+
+#import "ExampleCoreAudioDevice.h"
+
+@implementation ExampleCoreAudioDevice
+
+@end
