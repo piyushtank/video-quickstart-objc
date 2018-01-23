e17399a47058214e8bab24fe25ed37eb07d6f12e
Chris Eagleston <ceagleston@twilio.com>
2018-01-23 09:48:02 -0800

    Stub out the TVIAudioDevice protocol.

--- ObjCVideoQuickstart/ExampleCoreAudioDevice.h
+++ ObjCVideoQuickstart/ExampleCoreAudioDevice.h
@@ -5,8 +5,30 @@
 //  Copyright © 2018 Twilio, Inc. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <TwilioVideo/TwilioVideo.h>
 
-@interface ExampleCoreAudioDevice : NSObject
+/*
+ *  ExampleCoreAudioDevice uses a RemoteIO audio unit to playback stereo audio at up to 48 kHz.
+ *  In contrast to `TVIDefaultAudioDevice`, this device does not record audio and is intended for high quality playback
+ *  only. Since playback and recording is not needed this device does not use the built in echo cancellation provided
+ *  by the VoiceProcessingIO audio unit.
+ */
+@interface ExampleCoreAudioDevice : NSObject <TVIAudioDevice>
+
+- (nullable TVIAudioFormat *)renderFormat;
+
+- (BOOL)initializeRenderer;
+
+- (BOOL)startRendering:(nonnull TVIAudioDeviceContext)context;
+
+- (BOOL)stopRendering;
+
+- (nullable TVIAudioFormat *)captureFormat;
+
+- (BOOL)initializeCapturer;
+
+- (BOOL)startCapturing:(nonnull TVIAudioDeviceContext)context;
+
+- (BOOL)stopCapturing;
 
 @end
--- ObjCVideoQuickstart/ExampleCoreAudioDevice.m
+++ ObjCVideoQuickstart/ExampleCoreAudioDevice.m
@@ -7,6 +7,61 @@
 
 #import "ExampleCoreAudioDevice.h"
 
+@interface ExampleCoreAudioDevice()
+
+@property (nonatomic, assign) TVIAudioDeviceContext renderingContext;
+
+@end
+
 @implementation ExampleCoreAudioDevice
+
+#pragma mark - TVIAudioDeviceRenderer.
+
+- (nullable TVIAudioFormat *)renderFormat {
+    // TODO: Use the real value from AVAudioSession here!
+    TVIAudioFormat *format = [[TVIAudioFormat alloc] initWithChannels:TVIAudioChannelsStereo
+                                                           sampleRate:TVIAudioSampleRate48000
+                                                      framesPerBuffer:TVIAudioSampleRate48000 / 100];
+
+    return format;
+}
+
+- (BOOL)initializeRenderer {
+    // Setup AVAudioSession and size buffers here.
+    return YES;
+}
+
+- (BOOL)startRendering:(nonnull TVIAudioDeviceContext)context {
+    self.renderingContext = context;
+
+    // TODO: Start core audio graph here.
+
+    return YES;
+}
+
+- (BOOL)stopRendering {
+    // TODO: Stop and destroy core audio graph here.
+    return YES;
+}
+
+#pragma mark - TVIAudioDeviceCapturer.
+
+- (nullable TVIAudioFormat *)captureFormat {
+    // We don't support capturing, and return a nil format to indicate this. The other TVIAudioDeviceCapturer methods
+    // are simply stubs.
+    return nil;
+}
+
+- (BOOL)initializeCapturer {
+    return NO;
+}
+
+- (BOOL)startCapturing:(nonnull TVIAudioDeviceContext)context {
+    return NO;
+}
+
+- (BOOL)stopCapturing {
+    return NO;
+}
 
 @end
