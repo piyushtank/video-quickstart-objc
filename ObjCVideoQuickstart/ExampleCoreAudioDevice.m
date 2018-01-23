8eee490540c62cd6bf797c64a85e01af23eec374
Chris Eagleston <ceagleston@twilio.com>
2018-01-23 11:50:03 -0800

    Setup and run the AudioUnit.

--- ObjCVideoQuickstart/ExampleCoreAudioDevice.m
+++ ObjCVideoQuickstart/ExampleCoreAudioDevice.m
@@ -9,6 +9,9 @@
 
 // We want to get as close to 10 msec buffers as possible, because this is what the media engine prefers.
 static double kPreferredIOBufferDuration = 0.01;
+// The RemoteIO audio unit uses bus 0 for ouptut, and bus 1 for input.
+static int kOutputBus = 0;
+static int kInputBus = 1;
 
 @interface ExampleCoreAudioDevice()
 
@@ -31,26 +34,27 @@ static double kPreferredIOBufferDuration = 0.01;
 
 - (nullable TVIAudioFormat *)renderFormat {
     // TODO: Use the real value from AVAudioSession here!
-    TVIAudioFormat *format = [[TVIAudioFormat alloc] initWithChannels:TVIAudioChannelsStereo
-                                                           sampleRate:TVIAudioSampleRate48000
-                                                      framesPerBuffer:TVIAudioSampleRate48000 / 100];
+    if (!_renderingFormat) {
+        _renderingFormat = [[TVIAudioFormat alloc] initWithChannels:TVIAudioChannelsStereo
+                                                         sampleRate:TVIAudioSampleRate48000
+                                                    framesPerBuffer:TVIAudioSampleRate48000 / 100];
+    }
 
-    return format;
+    return _renderingFormat;
 }
 
 - (BOOL)initializeRenderer {
-    // Setup AVAudioSession and size buffers here.
+    [self setupAVAudioSession];
+
     return YES;
 }
 
 - (BOOL)startRendering:(nonnull TVIAudioDeviceContext)context {
     self.renderingContext = context;
 
-    // TODO: Start core audio graph here.
+    // Setup the Core Audio graph for playback.
     NSAssert(self.audioUnit == NULL, @"The audio unit should not be created yet.");
-    [self setupAudioUnit];
-
-    return YES;
+    return [self setupAudioUnit];
 }
 
 - (BOOL)stopRendering {
@@ -78,7 +82,24 @@ static double kPreferredIOBufferDuration = 0.01;
     return NO;
 }
 
-#pragma mark - Private
+#pragma mark - Private (AudioUnit callbacks)
+
+static OSStatus playout_cb(void *refCon,
+                           AudioUnitRenderActionFlags *actionFlags,
+                           const AudioTimeStamp *timestamp,
+                           UInt32 busNumber,
+                           UInt32 numFrames,
+                           AudioBufferList *bufferList)
+{
+    TVIAudioDeviceContext *context = (TVIAudioDeviceContext *)refCon;
+
+    assert(bufferList->mBuffers[0].mNumberChannels == 2);
+    readRenderData(context, bufferList->mBuffers[0].mData, bufferList->mBuffers[0].mDataByteSize);
+
+    return 0;
+}
+
+#pragma mark - Private (AVAudioSession and CoreAudio)
 
 - (void)setupAVAudioSession {
     AVAudioSession *session = [AVAudioSession sharedInstance];
@@ -118,8 +139,75 @@ static double kPreferredIOBufferDuration = 0.01;
     }
 }
 
-- (void)setupAudioUnit {
-    // TODO:
+- (BOOL)setupAudioUnit {
+    // Find and instantiate the RemoteIO audio unit.
+    AudioComponentDescription audioUnitDescription;
+    audioUnitDescription.componentType = kAudioUnitType_Output;
+    audioUnitDescription.componentSubType = kAudioUnitSubType_RemoteIO;
+    audioUnitDescription.componentManufacturer = kAudioUnitManufacturer_Apple;
+    audioUnitDescription.componentFlags = 0;
+    audioUnitDescription.componentFlagsMask = 0;
+
+    AudioComponent audioComponent = AudioComponentFindNext(NULL, &audioUnitDescription);
+
+    OSStatus status = AudioComponentInstanceNew(audioComponent, &_audioUnit);
+    if (status != 0) {
+        NSLog(@"Could not find RemoteIO AudioComponent instance!");
+        return NO;
+    }
+
+    // Configure the RemoteIO audio unit.
+    AudioStreamBasicDescription streamDescription = self.renderingFormat.streamDescription;
+
+    UInt32 enableOutput = 1;
+    status = AudioUnitSetProperty(_audioUnit, kAudioOutputUnitProperty_EnableIO,
+                                  kAudioUnitScope_Output, kOutputBus,
+                                  &enableOutput, sizeof(enableOutput));
+    if (status != 0) {
+        NSLog(@"Could not enable output bus!");
+        return NO;
+    }
+
+    // TODO: Should this be on the output scope?
+    status = AudioUnitSetProperty(_audioUnit, kAudioUnitProperty_StreamFormat,
+                                  kAudioUnitScope_Input, kOutputBus,
+                                  &streamDescription, sizeof(streamDescription));
+    if (status != 0) {
+        NSLog(@"Could not enable output bus!");
+        return NO;
+    }
+
+    // Disable input, we don't want it.
+    UInt32 enableInput = 0;
+    status = AudioUnitSetProperty(_audioUnit, kAudioOutputUnitProperty_EnableIO,
+                                  kAudioUnitScope_Input, kInputBus, &enableInput,
+                                  sizeof(enableInput));
+
+    if (status != 0) {
+        NSLog(@"Could not disable input bus!");
+        return NO;
+    }
+
+    // Setup the rendering callback.
+    AURenderCallbackStruct renderCallback;
+    renderCallback.inputProc = playout_cb;
+    renderCallback.inputProcRefCon = (void *)(self.renderingContext);
+    status = AudioUnitSetProperty(_audioUnit, kAudioUnitProperty_SetRenderCallback,
+                                  kAudioUnitScope_Input, kOutputBus, &renderCallback,
+                                  sizeof(renderCallback));
+    if (status != 0) {
+        NSLog(@"Could not set rendering callback!");
+        return NO;
+    }
+
+    // Finally, initialize the RemoteIO audio unit.
+    status = AudioUnitInitialize(_audioUnit);
+    if (status != 0) {
+        NSLog(@"Could not initialize the audio unit!");
+        return NO;
+    }
+
+    return YES;
 }
 
 - (void)registerAVAudioSessionObservers {
@@ -127,7 +215,7 @@ static double kPreferredIOBufferDuration = 0.01;
 }
 
 - (void)unregisterAVAudioSessionObservers {
-    // TODO:
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (void)teardownAudioUnit {
